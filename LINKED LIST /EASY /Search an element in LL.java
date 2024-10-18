617b6a27309f241197d7c0cee660e1b053681feb
PidadhDinesh5095 <161049822+PidadhDinesh5095@users.noreply.github.com>
2024-10-18 23:56:44 +0530

    Create Search an element in LL.java

---
+++ LINKED LIST /EASY /Search an element in LL.java
@@ -0,0 +1,24 @@
+
+
+
+
+// User function Template for Java
+
+/* Node of a linked list
+  class Node {
+   int data;
+    Node next;
+    Node(int d)  { data = d;  next = null; }
+}
+*/
+class Solution {
+    static boolean searchKey(int n, Node head, int key) {
+        while(head.next!=null){
+            if(head.data==key){
+                return true;
+            }
+            head=head.next;
+        }
+        return false;
+    }
+}

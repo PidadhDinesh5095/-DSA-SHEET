f1adb33f3d8efb50885885817036137e97bc648b
PidadhDinesh5095 <161049822+PidadhDinesh5095@users.noreply.github.com>
2024-10-19 00:36:05 +0530

    Create Detect.java

---
+++ LINKED LIST /EASY /Detect a loop /Detect.java
@@ -0,0 +1,28 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+      if(head==null || head.next==null){
+        return false;
+        }
+      ListNode a=head;
+      ListNode b=head.next;
+      while(a!=b){
+        if(b==null || b.next==null){
+         return false;
+        }
+        a=a.next;
+        b=b.next.next;
+      }
+      return true;
+    }
+}
